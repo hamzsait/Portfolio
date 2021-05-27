7b0db1387bcdebd0e3cd44f1513bc6157be89b69
hamzsait <hamzasait@utexas.edu>
2021-05-26 23:30:16 -0500

    added outline for Jumbotron on homepage

--- src/components/ProfileWindow.js
+++ src/components/ProfileWindow.js
@@ -1,11 +1,23 @@
 import { Component } from 'react'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Button from 'react-bootstrap/Button'
+import '../css/ProfileWindow.css'
 
 class ProfileWindow extends Component {
 
     render() {
         return (
-            <div>
-                <h1> Testing </h1>
+            <div id="profileWindow" >
+                <Jumbotron>
+                    <h1>Hello, world!</h1>
+                    <p>
+                        This is a simple hero unit, a simple jumbotron-style component for calling
+                        extra attention to featured content or information.
+                    </p>
+                    <p>
+                        <Button variant="primary">Learn more</Button>
+                    </p>
+                </Jumbotron>
             </div>
         )
     }
