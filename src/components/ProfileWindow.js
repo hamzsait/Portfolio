798a5d37df9401ab1f0167e96835dec60eb09d13
hamzsait <hamzasait@utexas.edu>
2021-05-27 09:53:00 -0500

    added profile photo to jumbotron

--- src/components/ProfileWindow.js
+++ src/components/ProfileWindow.js
@@ -8,15 +8,20 @@ class ProfileWindow extends Component {
     render() {
         return (
             <div id="profileWindow" >
-                <Jumbotron>
-                    <h1>Hello, world!</h1>
-                    <p>
-                        This is a simple hero unit, a simple jumbotron-style component for calling
-                        extra attention to featured content or information.
-                    </p>
-                    <p>
-                        <Button variant="primary">Learn more</Button>
-                    </p>
+                <Jumbotron id = "jumbotron">
+                    <div>
+                        <img alt = 'profilePhoto' src="../images/profilePhoto.jpeg"></img>
+                    </div>
+                    <div>
+                        <h1>Hello, world!</h1>
+                        <p>
+                            This is a simple hero unit, a simple jumbotron-style component for calling
+                            extra attention to featured content or information.
+                        </p>
+                        <p>
+                            <Button variant="primary">Learn more</Button>
+                        </p>
+                    </div>
                 </Jumbotron>
             </div>
         )
