9571369b605ffedc4ff21414d6aaa8fa4548961f
hamzsait <hamzasait@utexas.edu>
2021-05-26 22:24:52 -0500

    added outlien for profile window component

---
+++ src/components/ProfileWindow.js
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+
+class ProfileWindow extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1> Testing </h1>
+            </div>
+        )
+    }
+}
+
+export default ProfileWindow
