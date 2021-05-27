723d4608a22fbf506f04810955eb3aa064cff3c7
hamzsait <hamzasait@utexas.edu>
2021-05-27 09:52:41 -0500

    changed navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,29 +1,36 @@
-import { Nav } from 'react-bootstrap'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
 import { Component } from 'react'
 import '../css/Nav.css'
 
+function alertblue(){
+    document.getElementById("lineblue").classList.add('setBlue');
+}
 
+function alertred(){
+    document.getElementById("linered").classList.add('setRed');
+}
 
+function alertgreen(){
+    document.getElementById("linegreen").classList.add('setGreen');
+}
 class Navigation extends Component {
+
 
     render() {
         return (
             <div>
-                <Nav id = "navbar" defaultActiveKey="link-1">
-                    <Nav.Item >
-                        <Nav.Link href = '/' eventKey="link-1">Home</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link class = "navText" href = '/projects' eventKey="link-2">Projects</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link class = "navText" href = '/about' eventKey="link-3">About</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link class = "navText" href = '/contact' eventKey="link-4">Contact</Nav.Link>
-                    </Nav.Item>
-                </Nav>
-                {this.renderPage}
+                <Navbar id="navbar" bg="dark" variant="dark">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/home">Home</Nav.Link>
+                        <h1 id = 'lineblue' onMouseOver={() => alertblue()} class='nav-link lineblue'>|</h1>
+                        <Nav.Link href="/projects">Projects</Nav.Link>
+                        <h1 id = 'linegreen' onMouseOver={() => alertgreen()} class='nav-link linegreen'>|</h1>
+                        <Nav.Link href="/about">About</Nav.Link>
+                        <h1 id = 'linered' onMouseOver={() => alertred()} class='nav-link linered'>|</h1>
+                        <Nav.Link href="/contact">Contact</Nav.Link>
+                    </Nav>
+                </Navbar>
             </div>
         )
     }
