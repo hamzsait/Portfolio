f229ba21b4c4837e5f1b197a18bce5fc21adf104
hamzsait <hamzasait@utexas.edu>
2021-05-26 22:25:06 -0500

    added ProfileWindow component into Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from '../components/Navigation.js'
+import ProfileWindow from '../components/ProfileWindow.js'
 
 function Home() {
     return (
         <div>
             <Nav />
+            <ProfileWindow />
             <h1> Home </h1>
         </div>
     )
