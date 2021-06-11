cdc090716f684800c5185e806a9e9faaeaa49e7d
hamzsait <hamzasait@utexas.edu>
2021-06-11 18:44:34 -0500

    added id tage to Profile window for css to pickup

--- src/components/ProfileWindow.js
+++ src/components/ProfileWindow.js
@@ -9,11 +9,11 @@ class ProfileWindow extends Component {
         return (
             <div id="profileWindow" >
                 <Jumbotron id = "jumbotron">
-                    <div>
+                    <div id = 'profilePhoto'>
                         <img alt = 'profilePhoto' src="../images/profilePhoto.jpeg"></img>
                     </div>
-                    <div>
-                        <h1>Hello, world!</h1>
+                    <div id = 'profileText'>
+                        <h1>Hamza Sait</h1>
                         <p>
                             This is a simple hero unit, a simple jumbotron-style component for calling
                             extra attention to featured content or information.
