6ca7354dd5e0867ba110fa7ae86916025f9155b5
hamzsait <hamzasait@utexas.edu>
2021-06-11 18:56:42 -0500

    added classes and id tags to target css

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -14,21 +14,21 @@ function alertred(){
 function alertgreen(){
     document.getElementById("linegreen").classList.add('setGreen');
 }
+
 class Navigation extends Component {
-
-
+    
     render() {
         return (
             <div>
                 <Navbar id="navbar" bg="dark" variant="dark">
                     <Nav className="mr-auto">
                         <Nav.Link href="/home">Home</Nav.Link>
-                        <h1 id = 'lineblue' onMouseOver={() => alertblue()} class='nav-link lineblue'>|</h1>
-                        <Nav.Link href="/projects">Projects</Nav.Link>
-                        <h1 id = 'linegreen' onMouseOver={() => alertgreen()} class='nav-link linegreen'>|</h1>
-                        <Nav.Link href="/about">About</Nav.Link>
-                        <h1 id = 'linered' onMouseOver={() => alertred()} class='nav-link linered'>|</h1>
-                        <Nav.Link href="/contact">Contact</Nav.Link>
+                        <h1 id = 'lineblue' onMouseOver={() => alertblue()} className ='nav-link lineblue'>|</h1>
+                        <Nav.Link className = 'lineblue' id = 'blueText' href="/projects">Projects</Nav.Link>
+                        <h1 id = 'linegreen' onMouseOver={() => alertgreen()} className ='nav-link linegreen'>|</h1>
+                        <Nav.Link className = 'linegreen' id = 'greenText' href="/about">About</Nav.Link>
+                        <h1 id = 'linered' onMouseOver={() => alertred()} className ='nav-link linered'>|</h1>
+                        <Nav.Link id = 'redText' className = 'linered' href="/contact">Contact</Nav.Link>
                     </Nav>
                 </Navbar>
             </div>
