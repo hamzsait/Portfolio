402201ccfedc2339deb209247fed0af927a3dbcc
hamzsait <hamzasait@utexas.edu>
2021-06-11 23:44:03 -0500

    added /home route to go to homepage

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         <div>
             <Router>
                 <Route exact path="/" component={Home} />
+                <Route exact path="/home" component={Home} />
                 <Route exact path="/projects" component={Projects} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/contact" component={Contact} />
