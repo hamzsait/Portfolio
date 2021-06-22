2d60e93de837e891d7539cffc6bafa954555e45d
hamzsait <hamzasait@utexas.edu>
2021-06-22 22:17:05 +0200

    added hover for navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -17,6 +17,19 @@ function alertgreen(){
     document.getElementById("linegreen").classList.add('setGreen');
 }
 
+function addLineBlue(){
+    document.getElementById("lineblue").classList.add('setBlue')
+}
+
+function addLineGreen(){
+    document.getElementById("linegreen").classList.add('setGreen')
+}
+
+function addLineRed(){
+    document.getElementById("linered").classList.add('setRed')
+}
+
+
 function Navigation() {
 
         return (
@@ -25,11 +38,11 @@ function Navigation() {
                     <Nav className="mr-auto">
                         <Nav.Link href="/home">Home</Nav.Link>
                         <h1 id = 'lineblue' onMouseOver={() => alertblue()} className ='nav-link lineblue'>|</h1>
-                        <Nav.Link className = 'lineblue' id = 'blueText' href="/projects">Projects</Nav.Link>
+                        <Nav.Link onMouseOver={() => addLineBlue()} className = 'lineblue' id = 'blueText' href="/projects">Projects</Nav.Link>
                         <h1 id = 'linegreen' onMouseOver={() => alertgreen()} className ='nav-link linegreen'>|</h1>
-                        <Nav.Link className = 'linegreen' id = 'greenText' href="/about">About</Nav.Link>
+                        <Nav.Link onMouseOver={() => addLineGreen()} className = 'linegreen' id = 'greenText' href="/about">About</Nav.Link>
                         <h1 id = 'linered' onMouseOver={() => alertred()} className ='nav-link linered'>|</h1>
-                        <Nav.Link id = 'redText' className = 'linered' href="/contact">Contact</Nav.Link>
+                        <Nav.Link onMouseOver={() => addLineRed()} id = 'redText' className = 'linered' href="/contact">Contact</Nav.Link>
                     </Nav>
                     <a href = 'https://github.com/hamzsait' id = 'icon'>
                             <Github/>
