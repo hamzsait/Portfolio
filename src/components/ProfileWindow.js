246a91cc16173668a57298b4ec1373b6086489b5
hamzsait <hamzasait@utexas.edu>
2021-06-26 17:41:26 +0200

    added hover function for jumbotron

--- src/components/ProfileWindow.js
+++ src/components/ProfileWindow.js
@@ -2,23 +2,25 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 import '../css/ProfileWindow.css'
 
 
-function borderChange(){
-    document.getElementById("jumbotron").classList.add('setBorder')
+function hoverJumbo(){
+    document.getElementById("profileText").classList.add('hoverJumbo')
+    console.log("yooo")
 }
 
 
 function ProfileWindow(){
 
         return (
-            <div onMouseHover={() => borderChange()} id="profileWindow" >
-                <Jumbotron id = "jumbotron">
+            <div id="profileWindow" >
+                <Jumbotron id = "jumbotron" onMouseOver={() => hoverJumbo()}>
                     <div id = 'profilePhoto'>
                         <img alt = 'profilePhoto' src="../images/profilePhoto.jpeg"></img>
                     </div>
                     <div id = 'profileText'>
                         <h1>Hamza Sait</h1>
-                        <p>
-                            Full Stack Web Developer 
+                        <p >
+                                Full Stack Web Developer and Data Engineer based in Austin, Texas focused on politics, policy and society.
+                            The majority of my work revolves around civic tech and how to improve society through software solutions. 
                         </p>
                     </div>
                 </Jumbotron>
